3c8596bb1774332142c0df78c1cc7f80591f1841
dohyekim <dhleen5@hanmail.net>
2019-03-29 13:09:10 +0900

    search

--- Search.py
+++ Search.py
@@ -9,6 +9,7 @@ class ElasticSearch():
     findEng = []
     findKor = []
     engtalk = []
+    kortalk = []
     def __init__ (self):
 
         # 전체 Talk의 수 구하기
@@ -80,6 +81,64 @@ class ElasticSearch():
             # kstrs = " ".join(krows)
             # self.findKor.append(kstrs)
         # print(self.findKor)
+
+    def getEng(self, search):
+        # 전체 row 수만큼 loop 돌리도록 수정
+        for t in range(1, 20):
+            sqlseleng = "select engcue from English where talk_id = {} order by engcue desc limit 1".format(t)
+            sqlselkor = "select korcue from Korean where talk_id = {} order by korcue desc limit 1".format(t)
+
+            sqlSearch = '''select talk_id, korcue, kor from English 
+                        where kor like '%{}%'
+                        and talk_id = {}'''.format(search, t)
+        
+            conn = f.get_conn()
+            cur = conn.cursor()
+            cur.execute(sqlseleng)
+            engcnt = cur.fetchall()
+            if len(engcnt) != 0:
+                engs = engcnt[0][0]
+            cur.execute(sqlselkor)
+            korcnt = cur.fetchall()
+            if len(korcnt) != 0:
+                kors = korcnt[0][0]
+            # print(abs(engcnt - korcnt))
+            if abs(engs - kors) > 3:
+                print(" Hard to match those two ")
+                continue
+
+            else:
+                cur.execute(sqlSearch)
+                rows = cur.fetchall()
+                if len(rows) == 0:
+                    continue
+                else:
+                    print("rows >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ", rows)
+                    for i in range(len(rows)):
+                        # (talk_id, cue, english)
+                        talkcue = (rows[i][0], rows[i][1], rows[i][2])
+                        self.kortalk.append(talkcue)
+                    print(self.kortalk)
+                print("--------------- NEXT TURN -----------------")
+
+
+    def engequiv(self):
+        for k in range(len(self.kortalk)):
+            cue = self.kortalk[k][1]
+            if self.kortalk[k][1] == 1:
+                sqlEngSearch = '''select engcue, eng from English 
+                    where engcue between {} and {}
+                    and talk_id = {}'''.format(cue, cue+2, self.kortalk[k][0])
+            else:     
+                sqlEngSearch = '''select engcue, eng from English 
+                    where engcue between {} and {}
+                    and talk_id = {}'''.format(cue-2, cue+2, self.kortalk[k][0])
+            conn = f.get_conn()
+            cur = conn.cursor()
+            cur.execute(sqlEngSearch)
+            krows = cur.fetchall() # tuple
+            print(krows)
+
 
 
 s = ElasticSearch()
